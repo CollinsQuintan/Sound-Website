6c90a39da2391f3544c4b1b8349a2a9631a54e64
CollinsQuintan <collinscheruiyot15@gmail.com>
2023-03-30 16:34:32 +0300

    Added animation to button press

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (var i = 0;i<numOfDrumButtons; i++){
       var buttonInnerHTML = this.innerHTML;
       
       makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
     });
 }
 
@@ -48,6 +49,16 @@ function makeSound(key){
             console.log(innerHTML);
             break;
       } 
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." +currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100
+
+    );
 }
 
 
